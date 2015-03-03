4c9f4ce1bf1a1f2c685df09b9db324e3e9a84dfb
Mike Allison <mikeallison79@gmail.com>
2015-03-03 17:03:30 -0500

    Started Chapter 1

---
+++ 01_the_basics/03.rb
@@ -0,0 +1,7 @@
+movies = { 'Jaws' => 1975, 'Million Dollar Baby' => 2004,  'Man of Steel' => 2013, 'Black Hawk Down' => 2001, 'The Evil Dead' => 1981 }
+
+puts movies['Jaws']
+puts movies['Million Dollar Baby']
+puts movies['Man of Steel']
+puts movies['Black Hawk Down']
+puts movies['The Evil Dead']
