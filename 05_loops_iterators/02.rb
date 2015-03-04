2131297acf36f687384b2271d5c1e8b8749980dd
Mike Allison <mikeallison79@gmail.com>
2015-03-03 21:40:18 -0500

    Completed Chapter 5

---
+++ 05_loops_iterators/02.rb
@@ -0,0 +1,6 @@
+cmd = gets.chomp
+
+while cmd != 'STOP'
+  puts "You didn't type STOP"
+  cmd = gets.chomp
+end
---
+++ 05_loops_iterators/03.rb
@@ -0,0 +1,3 @@
+guitars = ["Strat", "Les Paul", "Telecaster", "Flying V"]
+
+guitars.each_with_index { |guitar, index| puts "#{index} #{guitar}" }
