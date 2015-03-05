fc4590569adb6653b506b132ce0f123adb558078
Mike Allison <mikeallison79@gmail.com>
2015-03-04 19:10:36 -0500

    Worked on Chapter 7

--- 07_hashes/06.rb
+++ 07_hashes/06.rb
@@ -2,7 +2,23 @@ words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
 
+word_positions = []
+unique_words = []
+
+# For each word in [words]
+# Split each word into single letters, sort the letters, join, push onto [word_positions] (leave this unchanged)
 words.each do |word|
-  sorted = word.split('').sort.join
-  
+  word_positions << word.split('').sort.join
 end
+
+# Run .uniq! on [word_positions] to push onto [unique_words]
+unique_words << word_positions.uniq
+
+# For each word in [unique_words]
+unique_words.each do |word|
+
+end
+
+# Get the indexes for each word in [positions]
+# Push onto [anagrams] = words[index]
+# Return [anagrams]
